17a706fb123ddf162ecbeb9cf4a144a9250d0298
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2026-09-17 19:57:55 -0300

    feat: bootstrap statuses categories and technologies

--- public/js/services/bootstrap-service.js
+++ public/js/services/bootstrap-service.js
@@ -1,8 +1,15 @@
+import { categoryRepository } from "../repositories/category-repository.js";
 import { projectStatusRepository } from "../repositories/project-status-repository.js";
+import { technologyRepository } from "../repositories/technology-repository.js";
 
 async function initializeUserWorkspace(uid) {
   if (!uid) throw new Error("UID é obrigatório para inicializar o workspace.");
-  await projectStatusRepository.ensureDefaults(uid);
+
+  await Promise.all([
+    projectStatusRepository.ensureDefaults(uid),
+    categoryRepository.ensureDefaults(uid),
+    technologyRepository.ensureDefaults(uid)
+  ]);
 }
 
 export { initializeUserWorkspace };
