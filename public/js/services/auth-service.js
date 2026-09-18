1da534546cb7c4e27b81e563d9cee507d2c330fd
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2026-09-18 06:50:24 -0300

    security: restrict Google login to approved UIDs

--- public/js/services/auth-service.js
+++ public/js/services/auth-service.js
@@ -6,8 +6,32 @@ import {
 
 import { auth, googleProvider } from "../config/firebase.js";
 
+const ALLOWED_USER_UIDS = Object.freeze([
+  "056LHaaeFKYeqYCvNRUwbmww8AH3",
+  "sGuYhgdQswdtlhCTTfizJQDhNiF3"
+]);
+
+const allowedUserUids = new Set(ALLOWED_USER_UIDS);
+
+function isAuthorizedUser(user) {
+  return Boolean(user?.uid && allowedUserUids.has(user.uid));
+}
+
+function createUnauthorizedError() {
+  const error = new Error("Esta conta Google não está autorizada a acessar o EslavaHub.");
+  error.code = "auth/not-authorized";
+  return error;
+}
+
 async function signInWithGoogle() {
-  return signInWithPopup(auth, googleProvider);
+  const result = await signInWithPopup(auth, googleProvider);
+
+  if (!isAuthorizedUser(result.user)) {
+    await signOut(auth);
+    throw createUnauthorizedError();
+  }
+
+  return result;
 }
 
 async function signOutCurrentUser() {
@@ -15,10 +39,23 @@ async function signOutCurrentUser() {
 }
 
 function observeAuthState(callback) {
-  return onAuthStateChanged(auth, callback);
+  return onAuthStateChanged(auth, async (user) => {
+    if (user && !isAuthorizedUser(user)) {
+      try {
+        await signOut(auth);
+      } finally {
+        callback(null);
+      }
+      return;
+    }
+
+    callback(user);
+  });
 }
 
 export {
+  ALLOWED_USER_UIDS,
+  isAuthorizedUser,
   observeAuthState,
   signInWithGoogle,
   signOutCurrentUser
