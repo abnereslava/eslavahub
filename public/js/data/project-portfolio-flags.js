aab6e8b9be8a97c48b7216ad45707c8c469daea7
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2026-09-18 11:51:01 -0300

    data: define portfolio projects

---
+++ public/js/data/project-portfolio-flags.js
@@ -0,0 +1,15 @@
+const PORTFOLIO_FLAG_TARGET_UID = "056LHaaeFKYeqYCvNRUwbmww8AH3";
+
+const PORTFOLIO_PROJECT_FLAGS = Object.freeze([
+  { legacy_key: "0003-cristalizando", project_name: "Cristalizando" },
+  { legacy_key: "0022-teacher-invest", project_name: "Teacher Invest" },
+  { legacy_key: "0025-sara-santos-nutricionista", project_name: "Sara Santos Nutricionista" },
+  { legacy_key: "0002-liscano-faz-tudo", project_name: "Liscano Faz•Tudo" },
+  { legacy_key: "0012-ibv-pinhais", project_name: "IBV Pinhais" },
+  { legacy_key: "0017-to-doos", project_name: "To doOS" }
+]);
+
+export {
+  PORTFOLIO_FLAG_TARGET_UID,
+  PORTFOLIO_PROJECT_FLAGS
+};
