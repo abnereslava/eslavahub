00d7406c7be88623428b7e9c457d68491e936fee
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2026-09-18 11:07:54 -0300

    data: map Search Console URLs to projects

---
+++ public/js/data/project-search-console-links.js
@@ -0,0 +1,33 @@
+const SEARCH_CONSOLE_LINK_TARGET_UID = "056LHaaeFKYeqYCvNRUwbmww8AH3";
+
+const PROJECT_SEARCH_CONSOLE_LINKS = Object.freeze([
+  {
+    legacy_key: "0003-cristalizando",
+    project_name: "Cristalizando",
+    search_console_url:
+      "https://search.google.com/search-console?utm_source=about-page&resource_id=sc-domain:cristalizando.com.br"
+  },
+  {
+    legacy_key: "0001-eslava-solucoes-digitais",
+    project_name: "Eslava Soluções Digitais",
+    search_console_url:
+      "https://search.google.com/search-console?resource_id=sc-domain%3Aeslavasolucoesdigitais.com.br"
+  },
+  {
+    legacy_key: "0002-liscano-faz-tudo",
+    project_name: "Liscano Faz•Tudo",
+    search_console_url:
+      "https://search.google.com/search-console?resource_id=sc-domain%3Aliscanofaztudo.com.br"
+  },
+  {
+    legacy_key: "0025-sara-santos-nutricionista",
+    project_name: "Sara Santos Nutricionista",
+    search_console_url:
+      "https://search.google.com/search-console?resource_id=sc-domain%3Asarasantosnutri.com.br"
+  }
+]);
+
+export {
+  PROJECT_SEARCH_CONSOLE_LINKS,
+  SEARCH_CONSOLE_LINK_TARGET_UID
+};
