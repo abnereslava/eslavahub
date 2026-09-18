388a29c9bccfb00ebda115ad97e6700a1529a9b9
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2026-09-18 09:51:32 -0300

    data: map confirmed GitHub repositories to projects

---
+++ public/js/data/project-repository-links.js
@@ -0,0 +1,35 @@
+const REPOSITORY_LINK_TARGET_UID = "056LHaaeFKYeqYCvNRUwbmww8AH3";
+
+const PROJECT_REPOSITORY_LINKS = Object.freeze([
+  { legacy_key: "0001-eslava-solucoes-digitais", project_name: "Eslava Soluções Digitais", repository_url: "https://github.com/abnereslava/eslava_solucoes_digitais" },
+  { legacy_key: "0002-liscano-faz-tudo", project_name: "Liscano Faz•Tudo", repository_url: "https://github.com/abnereslava/Liscano-Faz-Tudo" },
+  { legacy_key: "0003-cristalizando", project_name: "Cristalizando", repository_url: "https://github.com/abnereslava/landingpage_cristalizando" },
+  { legacy_key: "0004-rpg-educacional", project_name: "RPG Educacional", repository_url: "https://github.com/abnereslava/rpg_perguntasv3" },
+  { legacy_key: "0008-corrida-educacional", project_name: "Corrida Educacional", repository_url: "https://github.com/abnereslava/race_gamev2" },
+  { legacy_key: "0009-duelo-educacional", project_name: "Duelo Educacional", repository_url: "https://github.com/abnereslava/duel_game" },
+  { legacy_key: "0010-festa-aniversario-vicente", project_name: "Festa de Aniversário Vicente", repository_url: "https://github.com/abnereslava/festa-vicente" },
+  { legacy_key: "0011-gerenciador-rpg-policial", project_name: "Gerenciador RPG Policial", repository_url: "https://github.com/abnereslava/gerenciador_rpg_policial" },
+  { legacy_key: "0012-ibv-pinhais", project_name: "IBV Pinhais", repository_url: "https://github.com/abnereslava/LandingPageIBVPinhais" },
+  { legacy_key: "0013-avaliatrilhas", project_name: "AvaliaTrilhas", repository_url: "https://github.com/abnereslava/avaliatrilhas" },
+  { legacy_key: "0014-blizpay", project_name: "Blizpay", repository_url: "https://github.com/abnereslava/Blizpay" },
+  { legacy_key: "0015-dostais", project_name: "Dostais", repository_url: "https://github.com/abnereslava/appdobb" },
+  { legacy_key: "0016-selah", project_name: "Selah", repository_url: "https://github.com/abnereslava/SelahApp" },
+  { legacy_key: "0017-to-doos", project_name: "To doOS", repository_url: "https://github.com/abnereslava/RotinaOS" },
+  { legacy_key: "0018-vallor-nest", project_name: "Vallor.nest", repository_url: "https://github.com/abnereslava/Gest-o-Financeira" },
+  { legacy_key: "0022-teacher-invest", project_name: "Teacher Invest", repository_url: "https://github.com/abnereslava/landingpage_teacherinvest" },
+  { legacy_key: "0024-recreaeduca", project_name: "Recreaeduca", repository_url: "https://github.com/abnereslava/recreaeduca" },
+  { legacy_key: "0025-sara-santos-nutricionista", project_name: "Sara Santos Nutricionista", repository_url: "https://github.com/abnereslava/landingpage_sara_nutricionista" }
+]);
+
+const UNRESOLVED_REPOSITORY_LINKS = Object.freeze([
+  { legacy_key: "0020-gerenciador-manutencoes-carro", project_name: "Gerenciador Manutenções Carro", reason: "nenhum repositório correspondente localizado" },
+  { legacy_key: "0021-klein-holtz", project_name: "Klein Holtz", reason: "nenhum repositório correspondente localizado" },
+  { legacy_key: "0023-rpg-educacional-2", project_name: "RPG Educacional 2.0", reason: "rpg_animais é plausível, mas não há evidência suficiente para vincular automaticamente" },
+  { legacy_key: "0024-teacher-chell", project_name: "Teacher Chell", reason: "nenhum repositório correspondente localizado" }
+]);
+
+export {
+  PROJECT_REPOSITORY_LINKS,
+  REPOSITORY_LINK_TARGET_UID,
+  UNRESOLVED_REPOSITORY_LINKS
+};
