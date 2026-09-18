2537852691ac49c4da5a9b380ba7ca914be0cf65
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2026-09-18 11:52:09 -0300

    feat: version portfolio flag enrichment

--- public/js/domain/workspace-bootstrap.js
+++ public/js/domain/workspace-bootstrap.js
@@ -3,6 +3,7 @@ const WORKSPACE_BOOTSTRAP_VERSIONS = Object.freeze({
   legacy_migration_version: 1,
   repository_links_version: 1,
   search_console_links_version: 1,
+  portfolio_flags_version: 1,
   project_numbers_version: 1
 });
 
