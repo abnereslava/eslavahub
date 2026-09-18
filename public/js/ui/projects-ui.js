4917249bb5a27044f694457b15e5837f31239651
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2026-09-18 15:01:02 -0300

    fix: use correct Search Console icon in projects

--- public/js/ui/projects-ui.js
+++ public/js/ui/projects-ui.js
@@ -157,7 +157,7 @@ const WEB_LINK_ICON_DATA_URI =
   "data:image/webp;base64,UklGRo4AAABXRUJQVlA4TIEAAAAvL8ALEGDURpKjpTb8YcXuuvicm4mafwZuGykqLB4s7x8OfjHjnWCKTMR3gt9BErVZvWnZBlOlTdVdtTGWd1QZFuBostQU1RqaeGgW0eXFRBQHELfySvYGsHl7POMRft7xdcWv29vvI3xf8+cGfi7h5x5+ruLn9hX3ApnA77UfxgMA";
 const GITHUB_FAVICON_URL = "https://github.githubassets.com/favicons/favicon.svg";
 const SEARCH_CONSOLE_ICON_URL =
-  "https://www.gstatic.com/search-console/scfe/logo_search_console.svg";
+  "https://images.seeklogo.com/logo-png/62/1/google-search-console-icon-logo-png_seeklogo-624699.png";
 
 function siteIcon() {
   return `
