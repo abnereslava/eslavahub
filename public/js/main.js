6437bed75bbe2fdbc48e10cdf6bfee0a61ecd646
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2026-09-18 12:43:52 -0300

    ui: replace refresh icon with Lucide style

--- public/js/main.js
+++ public/js/main.js
@@ -182,8 +182,10 @@ function renderAuthenticatedShell(user, bootstrapError = null) {
           aria-label="Atualizar dados"
         >
           <svg class="header-refresh-icon" viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M20 11a8 8 0 1 0-2.35 5.65"></path>
-            <path d="M20 4v7h-7"></path>
+            <path d="M21 12a9 9 0 0 0-9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path>
+            <path d="M3 3v5h5"></path>
+            <path d="M3 12a9 9 0 0 0 9 9 9.75 9.75 0 0 0 6.74-2.74L21 16"></path>
+            <path d="M16 16h5v5"></path>
           </svg>
         </button>
         <div class="account-identity">
