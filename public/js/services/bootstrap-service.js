d2efa6b9138e0e1f3005b9fc69a4fe9ba88240d1
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2026-09-18 07:59:22 -0300

    feat: backfill sequential project numbers on bootstrap

--- public/js/services/bootstrap-service.js
+++ public/js/services/bootstrap-service.js
@@ -2,6 +2,7 @@ import { categoryRepository } from "../repositories/category-repository.js";
 import { projectStatusRepository } from "../repositories/project-status-repository.js";
 import { technologyRepository } from "../repositories/technology-repository.js";
 import { migrateLegacySpreadsheet } from "./legacy-migration-service.js";
+import { ensureProjectNumbers } from "./project-number-service.js";
 
 async function initializeUserWorkspace(uid) {
   if (!uid) throw new Error("UID é obrigatório para inicializar o workspace.");
@@ -13,6 +14,7 @@ async function initializeUserWorkspace(uid) {
   ]);
 
   await migrateLegacySpreadsheet(uid);
+  await ensureProjectNumbers(uid);
 }
 
 export { initializeUserWorkspace };
