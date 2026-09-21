a0497cb5e9410aa994414ebad1c269228479fbb9
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2026-09-21 20:01:33 -0300

    feat: add Apartamento Portinari to EslavaHub

--- public/js/data/curated-projects.js
+++ public/js/data/curated-projects.js
@@ -12,6 +12,18 @@ const CURATED_PROJECTS = Object.freeze([
     client_name: "Cristian Martinelli",
     technology: "Typescript",
     quick_notes: null
+  },
+  {
+    key: "apartamento-portinari",
+    category: "Landing Page",
+    name: "Apartamento Portinari",
+    status_code: "FUNCTIONAL",
+    repository_url: "https://github.com/abnereslava/anuncio_apartamento",
+    deploy_url: null,
+    deploy_provider: "Cloudflare Workers",
+    client_name: "Projeto Pessoal",
+    technology: "Typescript",
+    quick_notes: "Site de apresentação e venda do apartamento no Residencial Portinari, em Jardim Amélia, Pinhais/PR."
   }
 ]);
 
